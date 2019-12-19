d36b0c9c905f4857b12180b50c48e02d9ee6eebb
Elluru <rama.obu.elluru@dxc.com>
2019-12-19 19:58:58 +0530

    reddy.java

--- reddy.java
+++ reddy.java
@@ -2,7 +2,7 @@ public class reddy {
 
     public static void main(String[] args) {
       
-        System.out.println("Bangalore,Fucking-Hell");
+        System.out.println("Bangalore-ec,Fucking-Hell");
     }
 
 }
